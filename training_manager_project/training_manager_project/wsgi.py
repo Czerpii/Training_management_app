55b621bc19c52c03e8107f4487b8b6ad338a6758
Czerpi <m.czerpak04@gmail.com>
2024-01-19 15:02:46 +0100

    Rename project folders

--- training_manager_project/training_manager_project/wsgi.py
+++ training_manager_project/training_manager_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for training_management_app project.
+WSGI config for training_manager_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'training_management_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'training_manager_project.settings')
 
 application = get_wsgi_application()
